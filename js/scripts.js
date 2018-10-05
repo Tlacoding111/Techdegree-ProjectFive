95e8790d00f1ab60329ba92c1767e136edb095a3
tla111 <tlamarketing111@gmail.com>
2018-10-05 16:33:05 -0400

    made changes

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//Practice
+
+
+
+// alert("Hello, how are you?");
+// document.write("<h2>Welcome to JavaScript Basics</h2>");
+
+// var message = "Thank you for visiting our page";
+// alert(message);
+
+// var dayOfWeek = prompt("What day is it?");
+// alert("Today is " + dayOfWeek);
+
+var diceRoll = Math.floor(Math.random() * 6) + 1;
+alert("You rolled a " + diceRoll);
